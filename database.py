7d0a1f65c97ff8d20f5aa566d9f0c0b36b44b3c8
pyoung <paul.andy.young@gmail.com>
2014-10-30 22:36:52 -0700

    Added database.py file

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+DATABASE_URI = 'sqlite:///tmp/beer_app.db'
+
+engine = create_engine(DATABASE_URI, echo=False)
+#engine = create_engine('sqlite:///:memory:', echo=False)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    from models import *
+    Base.metadata.create_all(bind=engine)  
