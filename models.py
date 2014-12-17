b9e0d746218b1647a00d170bfb83fa8f133cf9e9
pyoung <paul.andy.young@gmail.com>
2014-12-16 18:08:32 -0800

    Added a few more tables/models to the database

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, Table
-from sqlalchemy import Boolean, DateTime
+from sqlalchemy import Boolean, DateTime, Interval, Float
 from sqlalchemy.orm import relationship, backref
 from flask.ext.login import UserMixin
 from database import Base
@@ -25,6 +25,7 @@ class Beer(Base):
     style = Column(String)
 
     brewers = relationship(User, secondary='brewers')
+    recipes = relationship("Recipe", backref="beer")
 
 
 class Brewer(Base):
@@ -34,3 +35,45 @@ class Brewer(Base):
     beer_id = Column(Integer, ForeignKey('beers.id'), primary_key=True)
     user = relationship(User, backref=backref('users'))
     beer = relationship(Beer, backref=backref('beers'))
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+
+    id = Column(Integer, primary_key=True)
+    steep_time = Column(Integer)
+    boil_time = Column(Integer)
+    brew_date = Column(Date)
+    secondary_date = Column(Date)
+    bottle_date = Column(Date)
+
+    beer_id = Column(Integer, ForeignKey('beers.id'))
+    malts = relationship("Malt", backref="recipe")
+    hops = relationship("Hop", backref="recipe")
+
+
+
+class Malt(Base):
+    __tablename__ = 'malts'
+
+    id = Column(Integer, primary_key=True)
+    variety = Column(String)
+    weight = Column(Float)
+    lbs_kg = Column(Boolean)
+    milled = Column(Boolean)
+    notes = Column(String)
+
+    recipe_id = Column(Integer, ForeignKey("recipes.id"))
+
+
+class Hop(Base):
+    __tablename__ = 'hops'
+
+    id = Column(Integer, primary_key=True)
+    variety = Column(String)
+    addition_time = Column(Integer)
+    weight = Column(Float)
+    oz_grams = Column(Boolean)
+    fresh = Column(Boolean)
+    notes = Column(String)    
+
+    recipe_id = Column(Integer, ForeignKey("recipes.id"))
--- tests.py
+++ tests.py
@@ -1,12 +1,13 @@
 from database import init_db, db_session
-from models import User, Beer, Brewer
-import datetime
+from models import User, Beer, Brewer, Recipe, Malt, Hop
+from datetime import date
 import unittest
 
 class TestCase(unittest.TestCase):
 
     def setUp(self):
         init_db()
+
         
     def test_input_data(self):
 
@@ -25,7 +26,16 @@ class TestCase(unittest.TestCase):
         brewer2 = Brewer(user=u2, beer=b1)
         brewer3 = Brewer(user=u1, beer=b2)
 
+        # Add a Recipe to a beer
+        recipe1 = Recipe(steep_time=60, boil_time=60, brew_date=date(2014, 10, 31),
+                         beer=b1)
+        malt1 = Malt(variety="Pale Malt LME", weight=5, recipe=recipe1)
+        malt2 = Malt(variety="Great Western 2-row", weight=4.5, recipe=recipe1)
+        hop1 = Hop(variety="Magnum", addition_time=60, weight=1, recipe=recipe1)
+        hop2 = Hop(variety="Cascade", addition_time=0, weight=1, recipe=recipe1)
+
         db_session.add_all([u1, u2, b1, b2, brewer1, brewer2, brewer3])
+        db_session.add_all([recipe1, malt1, malt2, hop1, hop2])
         db_session.commit()
         print "****Data Has Been Commited to DB****"
 
@@ -49,6 +59,21 @@ class TestCase(unittest.TestCase):
         print "Beer Name: ", beer1.name
         for u in beer1.brewers:
             print u.name
+
+    def test_query4(self):
+        #Pull a recipe
+        beer1 = db_session.query(Beer).filter_by(name="Chaisoin").first()
+        recipe1 = db_session.query(Recipe).filter_by(beer=beer1).first()
+        r1_malts = db_session.query(Malt).filter_by(recipe=recipe1).all()
+        r1_hops = db_session.query(Hop).filter_by(recipe=recipe1).all()
+        print beer1.name, " Recipe - Brewd On: ", recipe1.brew_date
+        print "Steep Time: ", recipe1.steep_time, " Boil Time: ", recipe1.boil_time
+        print "Malts:"
+        for malt in r1_malts:
+            print "    Variety: ", malt.variety, "Amount: ", malt.weight
+        print "Hops:"
+        for hop in r1_hops:
+            print "    Variety: ", hop.variety, "Amount: ", hop.weight
             
 
     def tearDown(self):
