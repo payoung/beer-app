8d6545428d3012ad8a5c931db409d32274c24ec6
pyoung <paul.andy.young@gmail.com>
2014-12-19 17:18:01 -0800

    added a server to code to collect sensor data

---
+++ sensor_server.py
@@ -0,0 +1,48 @@
+"""
+Simple socket server for testing
+default port 1313, use -p to change
+"""
+
+import SocketServer
+import json
+import sys, getopt
+
+
+def get_args(argv):
+    """ get sys args """
+    port = 1313
+    opts, args = getopt.getopt(argv[1:], "p:", ["port="])
+    for opt, arg in opts:
+        if opt in ('-p', '--port'):
+            port = arg
+    return port
+
+
+class MyTCPServer(SocketServer.ThreadingTCPServer):
+    allow_reuse_address = True
+
+
+class MyTCPServerHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        try:
+            data = json.loads(self.request.recv(1024).strip())
+            # process the data, i.e. print it:
+            with open('temp.json', 'w') as outfile:
+                json.dump(data, outfile)
+            print data
+            # send some 'ok' back
+            self.request.sendall(json.dumps({'return':'ok'}))
+        except Exception, e:
+            print "Exception while receiving message: ", e
+
+
+def main(argv):
+    """ set up the server """
+    port = get_args(argv)
+    ip_addr = '0.0.0.0'
+    server = MyTCPServer((ip_addr, 1313), MyTCPServerHandler)
+    print "Server established. listening on address: ", ip_addr, " port: ", port 
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main(sys.argv)
