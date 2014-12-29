b347c0abd2fb379a5319c59cb0276e186eba1fe3
pyoung <paul.andy.young@gmail.com>
2014-12-28 17:53:41 -0800

    added the navigation plugin to allow for panning and zooming in the plot

--- views.py
+++ views.py
@@ -5,8 +5,16 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-def c_to_f(tempC):
-    return (tempC*(9.0/5.0) + 32.0)
+
+def c_to_f(temp_c):
+    """" convert celcius to fahrenheit """
+    return temp_c*(9.0/5.0) + 32.0
+
+
+def rndup(num, n_power):
+    """ Round up to the 10**n power """
+    return num - (num % -10**n_power)
+
 
 @app.route('/')
 def what_is_the_temp():
@@ -17,12 +25,12 @@ def what_is_the_temp():
         tempreader = csv.reader(csvfile, delimiter=',')
         for row in tempreader:
             #dts.append(datetime.strptime(row[1], '%Y-%m-%d %H:%M:%S.%f'))
-            #temperatures.append(float(row[0]))
             tempdata.append([cnt, c_to_f(float(row[0]))])
             cnt += 1
     print tempdata[0:4]
-    return render_template('main.html', tempdata=tempdata)
-    #return "Datetime: " + date_time + " Temperature (C): " + temperature
+    max_x_axis = rndup(cnt, len(str(cnt))-2)  # Returns 
+    return render_template('main.html', tempdata=tempdata, max_x_axis=max_x_axis)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
